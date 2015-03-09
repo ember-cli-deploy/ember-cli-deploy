3be393702f7f8165960420816fe20f59b85e353f
joseph bartels <joebartels@hotmail.com>
2015-03-09 15:18:02 -0400

    Fix for incompatibility with execSync on win64 and certain node builds Fixes #90, Fixes #92
    Allow option to pass syncExec function to tagging adapter via constructor instead.

--- node-tests/unit/utilities/tagging/sha-test.js
+++ node-tests/unit/utilities/tagging/sha-test.js
@@ -1,5 +1,4 @@
 var expect            = require('chai').expect;
-var sinon             = require('sinon');
 var ShaTaggingAdapter = require('../../../../lib/utilities/tagging/sha');
 
 var getShortShaVersion = function(sha) {
@@ -10,32 +9,17 @@ var GIT_SHA           = '04b724a6c656a21795067f9c344d22532cf593ae';
 var GIT_SHA_SHORTENED = getShortShaVersion(GIT_SHA);
 
 describe('ShaTaggingAdapter', function() {
-  var sandbox;
-
-  beforeEach(function() {
-    sandbox = sinon.sandbox.create();
-    if (require('child_process').execSync) {
-      sandbox
-        .stub(require('child_process'), 'execSync')
-        .returns(GIT_SHA);
-    } else {
-      // Node 0.10
-      sandbox
-        .stub(require('execSync'), 'exec')
-        .returns({stdout: GIT_SHA});
-    }
-  });
-
-  afterEach(function() {
-    sandbox.restore();
-  });
+  var mockSyncExec = function(){
+    return {stdout: GIT_SHA};
+  }
 
   describe('#createTag', function() {
     it('returns a tag based on current git-sha and manifestName', function() {
       var manifestName   = 'ember-cli-deploy';
-      var expectedTag    = manifestName+':'+GIT_SHA_SHORTENED;
+      var expectedTag    = manifestName + ':' + GIT_SHA_SHORTENED;
       var revisionTagger = new ShaTaggingAdapter({
-        manifest: manifestName
+        manifest: manifestName,
+        syncExec: mockSyncExec
       });
 
       expect(revisionTagger.createTag()).to.eq(expectedTag);
