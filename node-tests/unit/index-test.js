832aee9ecfe0c7f5f60b2e924f583d25a8500b01
Mattia Gheda <ghedamat@gmail.com>
2016-01-11 12:51:17 -0500

    fixes to postBuildHook

---
+++ node-tests/unit/index-test.js
@@ -0,0 +1,48 @@
+var Promise  = require('ember-cli/lib/ext/promise');
+var assert  = require('ember-cli/tests/helpers/assert');
+
+describe ('ember-cli-deploy', function() {
+  var subject;
+  beforeEach(function() {
+    subject = require('../../index.js');
+  });
+  describe('postBuild', function() {
+    it('requires an app', function() {
+      assert(!subject.postBuild(), 'returns false');
+    });
+
+    it('requires a deployTarget', function() {
+      var context = {
+        app: {
+          options: {
+            emberCLIDeploy: {
+              runOnPostBuild: null
+            }
+          }
+        }
+      };
+
+      assert(!subject.postBuild.apply(context));
+    });
+
+    it('reads the config', function() {
+      var context = {
+        project: {
+          name: function() {return 'test-project';},
+          root: process.cwd(),
+          addons: []
+        },
+        app: {
+          options: {
+            emberCLIDeploy: {
+              runOnPostBuild: 'production',
+              configFile: 'node-tests/fixtures/config/deploy.js'
+            }
+          }
+        }
+      };
+
+      assert(subject.postBuild.apply(context));
+    });
+  });
+});
